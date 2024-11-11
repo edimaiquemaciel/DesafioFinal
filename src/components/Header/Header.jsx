2467ab5064a9ff3a8e8d36c57139539e0246695f
Edimaique Maciel <edimaiqueacacio@gmail.com>
2024-11-11 10:22:58 -0300

    iniciando desafio final

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import {Link} from 'react-router-dom';
+export default function Header(){
+    return(
+        <header>
+            <img src="" alt="logomarca" />
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/novidades">Novidades</Link>
+                    </li>
+                    <li>
+                        <Link to="/sobre">Sobre</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
